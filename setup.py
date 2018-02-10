2e67111de1fa7421941b8761fd40865bf56e48ba
DrPandemic <bipbip500@gmail.com>
2018-02-10 16:18:26 -0500

    Renames package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='battlerite_client',
+setup(name='battlerite-client',
       version='0.1',
       description='Battlerite REST API client',
       url='https://github.com/DrPandemic/battlerite-client',
